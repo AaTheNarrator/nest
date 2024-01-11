56edfe72f15f355ca5393c2e9dab1b9fef0ff356
The Narrator <dima13kor@gmail.com>
2024-01-11 08:54:21 +0000

    add postgres

--- src/warehouse/warehouse.service.ts
+++ src/warehouse/warehouse.service.ts
@@ -7,24 +7,24 @@ import {CreateProductsInWarehouseDto} from "./dto/create-productsInWarehouse.dto
 @Injectable()
 export class WarehouseService {
 
-    constructor(@InjectModel(Warehouse) private warehouseRepository: typeof Warehouse,
-                private productsService : ProductsService) {
-    }
+    // constructor(@InjectModel(Warehouse) private warehouseRepository: typeof Warehouse,
+    //             private productsService : ProductsService) {
+    // }
 
     async addProductToWarehouse(dto : CreateProductsInWarehouseDto, manufacturer_id : number){
-        let product = await this.productsService.getProductByProductName(dto.product_name)
+        // let product = await this.productsService.getProductByProductName(dto.product_name)
 
-        if(!product){
-            throw new HttpException('Такого продукта не существует', HttpStatus.NOT_FOUND)
-        }
+        // if(!product){
+        //     throw new HttpException('Такого продукта не существует', HttpStatus.NOT_FOUND)
+        // }
 
-        let final_data = {
-            manufacturer_id : manufacturer_id,
-            product_id: product.product_id,
-            amount_product: dto.amount_product
-        }
+        // let final_data = {
+        //     manufacturer_id : manufacturer_id,
+        //     product_id: product.product_id,
+        //     amount_product: dto.amount_product
+        // }
 
-        return await this.warehouseRepository.create(final_data)
+        // return await this.warehouseRepository.create(final_data)
     }
 
 
